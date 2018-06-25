a330dded288e52c00439b5c56419943ba39a2f08
git-abhishek <36498066+git-abhishek@users.noreply.github.com>
2018-06-25 15:41:16 -0400

    parallelization

--- yt/visualization/tests/test_line_plots.py
+++ yt/visualization/tests/test_line_plots.py
@@ -10,15 +10,14 @@ Tests for making line plots
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+from nose.tools import assert_raises
+
 import yt
 from yt.utilities.answer_testing.framework import \
-    requires_ds, \
-    data_dir_load, \
     GenericImageTest
-from yt.testing import fake_random_ds
-import os
-import tempfile
-import shutil
+from yt.testing import assert_equal, fake_random_ds
+from yt.visualization.line_plot import _validate_point
+
 
 def setup():
     """Test specific setup."""
@@ -34,48 +33,60 @@ def compare(ds, plot, test_prefix, decimals=12):
     test.prefix = test_prefix
     return test
 
-tri2 = "SecondOrderTris/RZ_p_no_parts_do_nothing_bcs_cone_out.e"
-
-@requires_ds(tri2)
 def test_line_plot():
-    ds = data_dir_load(tri2, kwargs={'step':-1})
-    fields = [field for field in ds.field_list if field[0] == 'all']
-    plot = yt.LinePlot(ds, fields, (0, 0, 0), (1, 1, 0), 1000)
+    ds = fake_random_ds(4)
+    fields = [field for field in ds.field_list if field[0] == 'stream']
+    field_labels = {f:f[1] for f in fields}
+    plot = yt.LinePlot(ds, fields, (0, 0, 0), (1, 1, 0), 1000,
+                       field_labels=field_labels)
+    plot.annotate_legend(fields[0])
+    plot.annotate_legend(fields[1])
+    plot.set_x_unit('cm')
+    plot.set_unit(fields[0], 'kg/cm**3')
+    plot.annotate_title(fields[0], "Density Plot")
     yield compare(ds, plot, "answers_line_plot")
 
-@requires_ds(tri2)
 def test_multi_line_plot():
-    ds = data_dir_load(tri2, kwargs={'step':-1})
-    fields = [field for field in ds.field_list if field[0] == 'all']
+    ds = fake_random_ds(4)
+    fields = [field for field in ds.field_list if field[0] == 'stream']
+    field_labels = {f: f[1] for f in fields}
     lines = []
-    lines.append(yt.LineBuffer(ds, [0.25, 0, 0], [0.25, 1, 0], 100, label='x = 0.25'))
-    lines.append(yt.LineBuffer(ds, [0.5, 0, 0], [0.5, 1, 0], 100, label='x = 0.5'))
-    plot = yt.LinePlot.from_lines(ds, fields, lines)
+    lines.append(yt.LineBuffer(ds, [0.25, 0, 0], [0.25, 1, 0], 100,
+                               label='x = 0.25'))
+    lines.append(yt.LineBuffer(ds, [0.5, 0, 0], [0.5, 1, 0], 100,
+                               label='x = 0.5'))
+    plot = yt.LinePlot.from_lines(ds, fields, lines, field_labels=field_labels)
+    plot.annotate_legend(fields[0])
+    plot.annotate_legend(fields[1])
     yield compare(ds, plot, "answers_multi_line_plot")
-
-def test_line_plot_methods():
-    # Perform I/O in safe place instead of yt main dir
-    tmpdir = tempfile.mkdtemp()
-    curdir = os.getcwd()
-    os.chdir(tmpdir)
-
-    ds = fake_random_ds(32)
-
-    plot = yt.LinePlot(ds, 'density', [0, 0, 0], [1, 1, 1], 512)
-    plot.annotate_legend('density')
-    plot.set_x_unit('cm')
-    plot.set_unit('density', 'kg/cm**3')
-    plot.save()
-
-    os.chdir(curdir)
-    # clean up
-    shutil.rmtree(tmpdir)
 
 def test_line_buffer():
     ds = fake_random_ds(32)
     lb = yt.LineBuffer(ds, (0, 0, 0), (1, 1, 1), 512, label='diag')
     lb['density']
-    lb['density'] = 0
     lb['velocity_x']
-    lb.keys()
+    assert_equal(lb['density'].size, 512)
+    lb['density'] = 0
+    assert_equal(lb['density'], 0)
+    assert_equal(set(lb.keys()), set(['density', 'velocity_x']))
     del lb['velocity_x']
+    assert_equal(set(lb.keys()), set(['density']))
+
+def test_validate_point():
+    ds = fake_random_ds(3)
+    with assert_raises(RuntimeError) as ex:
+        _validate_point(0, ds, start=True)
+    assert_equal(str(ex.exception), 'Input point must be array-like')
+
+    with assert_raises(RuntimeError) as ex:
+        _validate_point(ds.arr([[0], [1]], 'code_length'), ds, start=True)
+    assert_equal(str(ex.exception), 'Input point must be a 1D array')
+
+    with assert_raises(RuntimeError) as ex:
+        _validate_point(ds.arr([0, 1], 'code_length'), ds, start=True)
+    assert_equal(str(ex.exception),
+                 'Input point must have an element for each dimension')
+
+    ds = fake_random_ds([32, 32, 1])
+    _validate_point(ds.arr([0, 1], 'code_length'), ds, start=True)
+    _validate_point(ds.arr([0, 1], 'code_length'), ds)
